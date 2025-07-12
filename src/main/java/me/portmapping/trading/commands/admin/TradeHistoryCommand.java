81190860c87241a24a8931259b2caed3f029638e
portmappings <portmapping.net@gmail.com>
2025-07-12 15:29:23 -0400

    fix: can now also look for trade history of offline players

--- src/main/java/me/portmapping/trading/commands/admin/TradeHistoryCommand.java
+++ src/main/java/me/portmapping/trading/commands/admin/TradeHistoryCommand.java
@@ -1,6 +1,7 @@
 package me.portmapping.trading.commands.admin;
 
 import me.portmapping.trading.ui.admin.TradeHistoryMenu;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Named;
@@ -9,7 +10,7 @@ import revxrsal.commands.bukkit.annotation.CommandPermission;
 public class TradeHistoryCommand {
     @Command("tradehistory")
     @CommandPermission("tausch.admin")
-    public void onTradeHistoryCommand(Player self, @Named("target") Player target){
+    public void onTradeHistoryCommand(Player self, @Named("target") OfflinePlayer target){
         new TradeHistoryMenu(target.getUniqueId(), target.getName()).openMenu(self);
     }
 }
